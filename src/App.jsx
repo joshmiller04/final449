1a5104b052d46a4a18374170c1f446f054f1a991
davidwells24 <136669947+davidwells24@users.noreply.github.com>
2025-04-14 17:38:22 +0000

    added some css to homepage

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,12 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 import { createClient } from '@supabase/supabase-js'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-      <h1>Big Ten Football Future Matchups</h1>
+      <h1 class="header">Big Ten Football Future Matchups</h1>
 
       <p>Input a Big Ten Team</p>
       <input type="text" id="team" name="team" title="Input the name of a Big Ten University" placeholder="Ex: Michigan State University" />
