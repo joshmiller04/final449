87f5a9d3c14f9a8260990ed780b1a2e04fee7969
davidwells24 <136669947+davidwells24@users.noreply.github.com>
2025-04-14 18:43:27 +0000

    continuing work on routing

--- src/main.jsx
+++ src/main.jsx
@@ -2,10 +2,8 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-import Schedule from './Schedule.jsx'
-const opponent = ['Michigan', 'Ohio State', 'Penn State', 'Wisconsin', 'Iowa', 'Nebraska', 'Northwestern', 'Illinois', 'Purdue', 'Maryland', 'Rutgers']
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App></App>
-  </StrictMode>,
+    <App />
+  </StrictMode>
 )
