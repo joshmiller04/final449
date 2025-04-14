0a2a1f3d5bc6c9526271a3e0e73bc6b3eb7b42b9
Kundoty <banyuanjundao@gmail.com>
2025-04-14 15:33:31 -0400

    services folder correctly changed, currently display Matchup page

--- src/main.jsx
+++ src/main.jsx
@@ -2,10 +2,11 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-import Schedule from './Schedule.jsx'
+import Schedule from './schedule.jsx'
+import MatchupSchedule from './components/MatchupSchedule.jsx'
 const opponent = ['Michigan', 'Ohio State', 'Penn State', 'Wisconsin', 'Iowa', 'Nebraska', 'Northwestern', 'Illinois', 'Purdue', 'Maryland', 'Rutgers']
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App></App>
+    <MatchupSchedule team="Ohio State University"></MatchupSchedule>
   </StrictMode>,
 )
