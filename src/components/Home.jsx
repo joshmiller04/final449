185ccd55e3601774c86d37158bc70969ebf2e00f
Kundoty <banyuanjundao@gmail.com>
2025-04-24 14:44:37 -0400

    made a pop up box showing all big ten school names and a button activating the box

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,8 +1,10 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
+import './Home.css'
 
 function Home() {
   const navigate = useNavigate();
+  const [displaySchools, setDisplaySchools] = useState(false);
 
   useEffect(() => {
     const input = document.getElementById('team');
@@ -28,6 +30,8 @@ function Home() {
     }
   };
 
+  const handleShowSchools = () => { setDisplaySchools(!displaySchools) };
+
   return (
     <main id="main">
       <h1 className="header">2025-2026 Big Ten Football Matchups</h1>
@@ -41,9 +45,44 @@ function Home() {
         placeholder="Ex: Michigan State"
       />
 
-      <button onClick={handleButtonClick} style={{ marginTop: '1rem' }}>
-        Go
-      </button>
+      <div style={{
+        display: 'flex',
+        flexDirection: 'column',
+        gap: '1rem',
+        alignItems: 'center'
+      }}>
+        <button onClick={handleButtonClick}>Go</button>
+        <button onClick={handleShowSchools}>Show School Names</button>
+      </div>
+      <div className={`popupBox ${displaySchools ? 'active' : ''}`}>
+        <div className="schoolList">
+          <button onClick={handleShowSchools} className="closeBox">
+            X
+          </button>
+          <h2>Big Ten Schools</h2>
+          <ul>
+            <li>Illinois - University of Illinois</li>
+            <li>Indiana - Indian University</li>
+            <li>Iowa - University of Iowa</li>
+            <li>Maryland - University of Maryland</li>
+            <li>Michigan - University of Michigan</li>
+            <li>Michigan State - Michigan State University</li>
+            <li>Minnesota - University of Minnesota</li>
+            <li>Nebraska - University of Nebraska-Lincoln</li>
+            <li>Northwestern - Northwestern University</li>
+            <li>Ohio State - Ohio State University</li>
+            <li>Oregon - University of Oregon</li>
+            <li>Penn State - Pennsylvania State University</li>
+            <li>Purdue - Purdue University</li>
+            <li>Rutgers - Rutgers University-New Brunswick</li>
+            <li>UCLA - University of California Los Angeles</li>
+            <li>USC - University of Southern California</li>
+            <li>Wisconsin - University of Wisconsin-Madison</li>
+            <li>Washington - University of Washington</li>
+            <h4 style={{color: "blue"}}>Please input the left side school name</h4>
+          </ul>
+        </div>
+      </div>
     </main>
   );
 }
