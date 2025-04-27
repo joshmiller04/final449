a4df0dbbf8981df952df185c5f43048a00343312
Kundoty <banyuanjundao@gmail.com>
2025-04-26 22:31:34 -0400

    loading overlay background a bit darker

--- src/components/LoadingOverlay.jsx
+++ src/components/LoadingOverlay.jsx
@@ -1,6 +1,6 @@
 export const LoadingOverlay = () => {
     return (
-        <div className="fixed top-0 left-0 w-screen h-screen flex justify-center items-center z-99">
+        <div className="fixed top-0 left-0 w-screen h-screen flex justify-center items-center z-99 bg-black/50">
             <div className="px-8 py-8 border-8 border-solid border-white border-t-8 border-t-solid border-t-blue-400 rounded-full animate-[spin_1s_linear_infinite] " />
         </div>
     )
